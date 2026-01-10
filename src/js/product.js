cf7dc61c1a2769529423ca799fdb5dc54bcf0f40
Van Taylor <jacob.van.taylor@gmail.com>
2026-01-09 21:27:43 -0700

    Fixed the shopping cart issue. It's not the greatest solution (especially after looking at the example solution but it'll work for now)

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,22 @@
-import { setLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
 
-function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+async function countItemsInCart() {
+  if (getLocalStorage('itemsInCart') === null) {
+    setLocalStorage('itemsInCart', 1);
+  } else {
+    return Number(getLocalStorage('itemsInCart'));
+  }
+
+  return Number(getLocalStorage('itemsInCart'));
+}
+
+async function addProductToCart(product) {
+  let itemsInCart = await countItemsInCart();
+  setLocalStorage(`customer-cart-item${itemsInCart}`, product);
+  setLocalStorage('itemsInCart', itemsInCart + 1);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
