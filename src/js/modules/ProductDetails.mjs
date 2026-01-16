405a11cee840d12b72f1ef4f8ffe8110f1b7017c
Van Taylor <jacob.van.taylor@gmail.com>
2026-01-15 18:51:26 -0700

    Tiny adjustments.

--- src/js/modules/ProductDetails.mjs
+++ src/js/modules/ProductDetails.mjs
@@ -20,13 +20,39 @@ export default class ProductDetails {
     renderProductDetails() {
         displayProduct(this.product);
     }
+
+
+    async addProductToCart(product) {
+        let itemsInCart = await countItemsInCart();
+        setLocalStorage(`customer-cart-item${itemsInCart}`, product);
+        setLocalStorage('itemsInCart', itemsInCart + 1);
+    }
 }
 
 
+const brand = document.getElementById('productBrand');
+const name = document.getElementById('productName');
+const image = document.getElementById('productImage');
+const price = document.getElementById('productCardPrice');
+const color = document.getElementById('productColor');
+const description = document.getElementById('productDescription');
+const button = document.getElementById('addToCart');
 
 
-async function addProductToCart(product) {
-    let itemsInCart = await countItemsInCart();
-    setLocalStorage(`customer-cart-item${itemsInCart}`, product);
-    setLocalStorage('itemsInCart', itemsInCart + 1);
+async function displayProduct(category, id) {
+    const data = new ProductData(category);
+    const product = await data.findProductById(id)
+
+    brand.textContent = `${product.Brand.Name}`;
+    name.textContent = `${product.NameWithoutBrand}`;
+
+    image.setAttribute('src', product.Image);
+    image.setAttribute('alt', `Image of the ${product.name}`);
+
+    price.textContent = `${product.ListPrice}`;
+    color.textContent = `${product.Colors[0].ColorName}`;
+    description.innerHTML = `${product.DescriptionHtmlSimple}`;
+    button.dataset.id = product.Id;
+    button.dataset.category = category;
+
 }
