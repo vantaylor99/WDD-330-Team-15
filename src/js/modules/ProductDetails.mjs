806606ec3296032f38813736b6717d14245174b2
Van Taylor <jacob.van.taylor@gmail.com>
2026-01-15 18:27:44 -0700

    Fixing team activity.

--- src/js/modules/ProductDetails.mjs
+++ src/js/modules/ProductDetails.mjs
@@ -5,12 +5,11 @@ export default class ProductDetails {
     constructor(productId, dataSource) {
         this.productId = productId;
         this.dataSource = dataSource;
-        this.productId = {};
+        this.product = {};
     }
 
     async init() {
-        this.product = await this.dataSource.
-
+        this.product = await this.dataSource.findProductById(this.productId);
     }
 }
 
@@ -21,21 +20,4 @@ async function addProductToCart(product) {
     let itemsInCart = await countItemsInCart();
     setLocalStorage(`customer-cart-item${itemsInCart}`, product);
     setLocalStorage('itemsInCart', itemsInCart + 1);
-}
-
-
-export default class ProductData {
-    constructor(category) {
-        this.category = category;
-        this.path = `../json/${this.category}.json`;
-    }
-    getData() {
-        return fetch(this.path)
-            .then(convertToJson)
-            .then((data) => data);
-    }
-    async findProductById(id) {
-        const products = await this.getData();
-        return products.find((item) => item.Id === id);
-    }
 }
