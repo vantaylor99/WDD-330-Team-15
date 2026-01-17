6d3a22c5aa622d6af404808a6c82291fe81128f3
Van Taylor <jacob.van.taylor@gmail.com>
2026-01-16 21:34:08 -0700

    Finished up the Individual Activity

---
+++ src/js/modules/ProductList.mjs
@@ -0,0 +1,38 @@
+import { renderListWithTemplate } from "../utils.mjs";
+
+function productCardTemplate(product, category) {
+    return `<li class="product-card">
+                <a href="product_pages/index.html?category=${category}&id=${product.Id}">
+                <img src="${product.Image}" alt="Image of the product '${product.Name}'">
+                <h2 class="card__brand">${product.Brand.Name}</h2>
+                <h3 class="card__name">${product.NameWithoutBrand}</h3>
+                <p class="product-card__price">$${product.ListPrice}</p>
+                </a>
+            </li>`
+}
+
+
+
+export default class ProductList{
+    constructor(category, dataSource, listElement){
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+    }
+
+    async init() {
+        const list = await this.dataSource.getData();
+        this.renderList(list)
+    }
+
+    renderList(list){
+        renderListWithTemplate(
+            (item) => productCardTemplate(item, this.category),
+            this.listElement,
+            list 
+        );
+    }
+
+}
+
+
