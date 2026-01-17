73408e29fed783412f0535574820f1a09e0fca40
Van Taylor <jacob.van.taylor@gmail.com>
2026-01-17 16:51:02 -0700

    (fix)cart badge wouldn't work on first addition

--- src/js/modules/cartCountBadge.mjs
+++ src/js/modules/cartCountBadge.mjs
@@ -1,9 +1,21 @@
 const span = document.getElementById("cartBadge");
 const itemsInCartCount = localStorage.getItem("itemsInCartCount");
 
-document.addEventListener('DOMContentLoaded', updateCartBadge());
+document.addEventListener('DOMContentLoaded', updateCartBadge);
+
 
 export function updateCartBadge() {
+    const count = localStorage.getItem("itemsInCartCount") || 0;
+
+    if(count > 0){
+        span.style.display = 'flex'
+        span.textContent = count
+    }
+    else{
+        span.style.display = 'none';
+        span.textContent = '';
+    }
+
     if (itemsInCartCount != null) {
         const currentItemsInCartCount = localStorage.getItem('itemsInCartCount');
         span.style.display = 'flex';
