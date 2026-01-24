67850332ecf922d04fe51c3d8b166478843e769d
Van Taylor <van@pollensense.com>
2026-01-24 14:36:57 -0700

    (fix)
    production fixes

--- src/js/modules/ProductList.mjs
+++ src/js/modules/ProductList.mjs
@@ -2,7 +2,7 @@ import { renderListWithTemplate } from "../utils.mjs";
 
 function productCardTemplate(product, category) {
     return `<li class="product-card">
-                <a href="../product_pages/index.html?category=${category}&id=${product.Id}">
+                <a href="../product_pages/index.html?id=${product.Id}">
                 <img src="${product.Images.PrimaryExtraLarge}" alt="Image of the product '${product.Name}'">
                 <h2 class="card__brand">${product.Brand.Name}</h2>
                 <h3 class="card__name">${product.NameWithoutBrand}</h3>
