a9a2e38c112b36c9896d1dd81e7c0f01330bbb1e
Van Taylor <van@pollensense.com>
2026-01-24 14:45:08 -0700

    final local fixes
    testing production

--- src/js/cart.js
+++ src/js/cart.js
@@ -11,7 +11,7 @@ function cartItemTemplate(item) {
   const newItem = `<li class='cart-card divider'>
   <a href='#' class='cart-card__image'>
     <img
-      src='${item.Image}'
+      src='${item.Images.PrimaryExtraLarge}'
       alt='${item.Name}'
     />
   </a>
---
+++ src/js/checkout.js
@@ -0,0 +1,5 @@
+import { loadHeaderFooter } from './utils.mjs';
+import { updateCartBadge } from './modules/cartCountBadge.mjs';
+
+
+loadHeaderFooter(updateCartBadge);
